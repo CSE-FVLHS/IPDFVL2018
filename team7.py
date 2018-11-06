68e75ce6ad552bc99a13d665c991a26f778a6d94
Evan Dvorak <dvoraeva000@students.fvlhs.org>
2018-11-06 19:09:01 +0000

    Added Team7 code

--- team7.py
+++ team7.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Do it 2 em' # Only 10 chars displayed.
+strategy_name = 'Do it to em'
+strategy_description = 'It checks what the person decided to chose back 1 round, 2 rounds, 5 rounds, and 15 rounds and does it to them?'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,6 +17,15 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    if len(my_history)<= 15:
+      return ('b')
+    if their_history[-1] and their_history[-2] and their_history[-5]and their_history[-15] == 'c':
+      return ('b')
+    if their_history[-1] and their_history[-2] and their_history[-5] and their_history[-15] == 'b':
+      return ('b')
+    else:
+         return 'c'
+          
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -65,4 +74,7 @@ if __name__ == '__main__':
               # move('bbb', 'ccc', 0, 0) returns 'b'.
               my_score=0, 
               their_score=0,
-              result='b')             
+              result='b')    
+  
+    
+    
